3e5f8c17a9cc20c274cf89293afba2b45de25f4d
changcheng123-web <xiatian21111@gmail.com>
2025-06-29 12:40:53 +0800

    修复项目预览配置

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './ws-测试2',
   server: {
     port: 5173,
     host: true
   },
-  build: {
-    outDir: '../dist'
-  }
+  publicDir: 'ws-测试2'
 })
